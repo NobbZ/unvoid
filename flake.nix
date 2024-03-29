17a3484ccd6a3100c5c7f54dfe1e55d731307f2d
Norbert Melzer <timmelzer@gmail.com>
2024-03-29 19:56:17 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  outputs = {flake-parts, ...}@inputs: flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = import inputs.systems;
+
+    perSystem = {pkgs, system, ...}: let
+      pkgsWithOverlays = inputs.nixpkgs.legacyPackages.${system}.extend inputs.rust-overlay.overlays.default;
+
+      mkShellMini = import inputs.mini-shell pkgs;
+
+      rust = pkgs.rust-bin.stable."1.77.0".default.override (orig: {
+        extensions = [ "rust-src" ] ++ orig.extensions;
+      });
+    in {
+      _module.args.pkgs = pkgsWithOverlays;
+
+      formatter = pkgs.alejandra;
+
+      devShells.default = mkShellMini {
+        packages = builtins.attrValues {
+          inherit (pkgs) cargo-nextest cargo-audit cargo-deny cargo-tarpaulin rust-analyzer-unwrapped;
+          inherit (pkgs) nil pre-commit;
+          inherit rust;
+        };
+
+        RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
+      };
+    };
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    
+    mini-shell.url = "github:viperML/mkshell-minimal";
+    mini-shell.flake = false;
+
+    systems.url = "github:nix-systems/default-linux";
+    systems.flake = false;
+  };
+}
