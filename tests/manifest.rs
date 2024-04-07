0e3ee8819361d03966e9c632abb435afda6044b2
Norbert Melzer <timmelzer@gmail.com>
2024-04-07 15:37:22 +0200

    remove a dbg

--- tests/manifest.rs
+++ tests/manifest.rs
@@ -7,7 +7,7 @@ use unvoid::version::Version;
 fn minimal_seserializes_correctly_from_rune_file() {
     let source = Source::from_path("tests/manifests/simple.rn").unwrap();
 
-    let manifest = dbg!(Manifest::from_rune(source)).unwrap();
+    let manifest = Manifest::from_rune(source).unwrap();
 
     assert_eq!(manifest.name, "my-project");
     assert_eq!(manifest.version, Version::new(0, 1, 0));
