789773b8622ab21699dd9547acebe4839df26e8e
Norbert Melzer <timmelzer@gmail.com>
2024-04-12 14:22:10 +0200

    clean up rstest cases

--- src/static/author.rs
+++ src/static/author.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 use crate::rune::ty::author::Author as RuneAuthor;
 
-#[derive(Debug, Deserialize,  Clone)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum Author {
     String(String),
@@ -51,61 +51,56 @@ impl PartialEq<Author> for Author {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
+    use super::*;
     use pretty_assertions::assert_eq;
     use rstest::rstest;
+    use std::collections::HashMap;
 
-    use super::*;
+    lazy_static::lazy_static! {
+        static ref SIMPLE_AUTHOR: Author = Author::String("Alice".to_string());
+        static ref EMAIL_AUTHOR: Author = Author::Structured {
+            name: "Alice".to_string(),
+            email: Some("example@example.com".into()),
+        };
 
-    fn toml_from_str(input: &str) -> HashMap<String, Author> {
+    }
+
+    const TOML_SIMPLE: &str = r#"author = "Alice""#;
+    const YAML_SIMPLE: &str = r#"author: "Alice""#;
+    const JSON_SIMPLE: &str = r#"{"author": "Alice"}"#;
+
+    const TOML_WITH_MAIL: &str = r#"author = { name = "Alice", email = "example@example.com" }"#;
+    const YAML_WITH_MAIL: &str = r#"author: { name: "Alice", email: "example@example.com" }"#;
+    const JSON_WITH_MAIL: &str =
+        r#"{"author": { "name": "Alice", "email": "example@example.com" }}"#;
+
+    fn str2toml(input: &str) -> HashMap<String, Author> {
         toml::from_str(input).unwrap()
     }
 
-    fn yaml_from_str(input: &str) -> HashMap<String, Author> {
+    fn str2yaml(input: &str) -> HashMap<String, Author> {
         serde_yaml::from_str(input).unwrap()
     }
 
-    fn json_from_str(input: &str) -> HashMap<String, Author> {
+    fn str2json(input: &str) -> HashMap<String, Author> {
         serde_json::from_str(input).unwrap()
     }
 
     #[rstest]
-    #[case::toml_string("author = \"Alice\"", toml_from_str, Author::String("Alice".to_string()))]
-    #[case::toml_structured(
-        "author = { name = \"Alice\", email = \"example@example.com\" }",
-        toml_from_str,
-        Author::Structured {
-            name: "Alice".to_string(),
-            email: Some("example@example.com".into()),
-        },
-    )]
-    #[case::yaml_string("author: \"Alice\"", yaml_from_str, Author::String("Alice".to_string()))]
-    #[case::yaml_structured(
-        "author: { name: \"Alice\", email: \"example@example.com\" }",
-        yaml_from_str,
-        Author::Structured {
-            name: "Alice".to_string(),
-            email: Some("example@example.com".into()),
-        },
-    )]
-    #[case::json_string(r#"{"author": "Alice"}"#, json_from_str, Author::String("Alice".to_string()))]
-    #[case::json_structured(
-        r#"{"author": { "name": "Alice", "email": "example@example.com" }}"#,
-        json_from_str,
-        Author::Structured {
-            name: "Alice".to_string(),
-            email: Some("example@example.com".into()),
-        },
-    )]
+    #[case::toml_string(TOML_SIMPLE, str2toml, &SIMPLE_AUTHOR)]
+    #[case::toml_structured(TOML_WITH_MAIL, str2toml, &EMAIL_AUTHOR)]
+    #[case::yaml_string(YAML_SIMPLE, str2yaml, &SIMPLE_AUTHOR)]
+    #[case::yaml_structured(YAML_WITH_MAIL, str2yaml, &EMAIL_AUTHOR)]
+    #[case::json_string(JSON_SIMPLE, str2json, &SIMPLE_AUTHOR)]
+    #[case::json_structured(JSON_WITH_MAIL, str2json, &EMAIL_AUTHOR)]
     fn test_author_deserialisation(
         #[case] input: &str,
         #[case] from_str: fn(&str) -> HashMap<String, Author>,
-        #[case] expected: Author,
+        #[case] expected: &Author,
     ) {
         let map: HashMap<String, Author> = from_str(input);
         let author = map.get("author").unwrap();
 
-        assert_eq!(author, &expected);
+        assert_eq!(author, expected);
     }
 }
